d047fa283598b60cf3ff458d71db1c3b61a239c3
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-14 08:13:05 +0000

    changes

--- .ccls-cache/@home@runner@Learn-C/Programs@hello.c
+++ .ccls-cache/@home@runner@Learn-C/Programs@hello.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
-int main(void) {
+/*
+a multi line comment
+*/
+int main(void)
+{
   printf("Hello World! \n");
-  printf("Line 2 \n");
+  printf("Line 2 \n"); // line 2
   printf("Line 3 \n");
+  // printf("Line 4 \n");
   return 0;
 }
