e7936af0f7e87a4fe1df27f9c37b19be02fa3ce1
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-14 09:16:30 +0000

    changes

--- .ccls-cache/@home@runner@Learn-C/Programs@if_else.c
+++ .ccls-cache/@home@runner@Learn-C/Programs@if_else.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
   int grade = 0;
 
   printf("Enter grade: ");
   scanf("%d", &grade);
 
-  if (grade >= 50){
+  if (grade >= 50)
+  {
     printf("Pass\n");
     printf("Congrats!\n");
   }
-  else{
+  else
+  {
     printf("Fail\n");
     printf("Good luck next time!\n");
   }
