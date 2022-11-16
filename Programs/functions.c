bb2282fa290f7135fe70f9fbe1899df5bfca787d
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-16 14:04:43 +0000

    new changes

--- Programs/functions.c
+++ Programs/functions.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*
 double cube(double num) // outputs and accepts a double type number
 {
   double result = num * num * num;
@@ -7,13 +8,27 @@ double cube(double num) // outputs and accepts a double type number
   // or
   // return num * num * num;
 }
+*/
+
+int max(int num1, int num2, int num3)
+{
+  int result;
+  if (num1 >= num2 && num1 >= num3) result = num1;
+  
+  else if (num2 >= num1 && num2 >= num3) result = num2;
+  
+  else result = num3;
+  return result;
+}
 
 int main()
 {
   // sayHi("John", 40);
   // sayHi("Jenny", 39);
 
-  printf("Answer: %.4f \n", cube(5.0));
+  // printf("Answer: %.4f \n", cube(5.0));
+
+  printf("Max: %d \n", max(10, 19, 12));
   return 0;
   
 }
