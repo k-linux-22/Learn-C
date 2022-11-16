81cf3057b01021ea8c29a8c1b27b57c552d12f01
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-16 12:59:00 +0000

    added new ways

--- Programs/char_type.c
+++ Programs/char_type.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 int main(void){
-  char s = 'a';
+  /*char s = 'a';
 
   printf("char: ");
   scanf("%c", &s);
 
   printf("char: %c \n", s);
   printf("char as ASCII: %d \n", s);
+  */
+  
+  char str[10];
+  printf("Write a sentence: \n");
+  // scanf("%s", str); // it will only scan till a space occurs in a sentence
+  
+  // fgets(str, 10, stdin); // only takes 10 characters as Input from User, and is able to intake a whole string
+  
+  scanf("%[^\n]%*c", str); // ^\n tells to take input until a newline comes, %*c reads newline, * indicates that this newline character is discarded
+  
+  printf("%s \n", str);
   return 0;
 }
