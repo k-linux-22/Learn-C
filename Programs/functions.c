35cb80711a0521fcb21d03fdc5d7a6ace409374d
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-16 13:56:51 +0000

    new file

--- Programs/functions.c
+++ Programs/functions.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
+double cube(double num) // outputs and accepts a double type number
+{
+  double result = num * num * num;
+  return result;
+  // or
+  // return num * num * num;
+}
+
 int main()
 {
+  // sayHi("John", 40);
+  // sayHi("Jenny", 39);
 
+  printf("Answer: %.4f \n", cube(5.0));
   return 0;
+  
 }
 
 // void means the function doe not return any value
-void 
+// void sayHi(char name[], int age)
+// {
+//   printf("Hello %s, of age %d \n", name, age);
+// }
