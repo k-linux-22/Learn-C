7e6d166db0360a22831daf6fe57848f34ca53a2a
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-16 12:16:03 +0000

    new files

---
+++ Programs/math.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  printf("%f \n", pow(2, 2)); // 2 to the power 2 = 4
+  printf("%f \n", sqrt(81)); // square root of 81 = 9
+  printf("%f \n", ceil(36.356)); // round up to 37
+  printf("%f \n", floor(36.656)); // round down to 36
+  return 0;
+}
