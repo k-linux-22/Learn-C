fbc2f4f4f1acbe09e5ad77d6c5da23b27e7c4672
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-16 12:10:47 +0000

    backend changes

---
+++ .ccls-cache/@home@runner@Learn-C/Programs@maxnum_until_-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int num = 0, max = -1;
+  // until -1 is entered, loop will keep taking inputs
+  while (num != -1)
+  {
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num > max) // max number out of all digits entered
+    {
+      max = num;
+    }
+    printf("Current max: %d\n", max);
+  }
+  printf("Max: %d\n", max);
+
+  return 0;
+}
