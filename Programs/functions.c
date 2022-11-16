6d6ce5a96686a4b885e598d00af447f9799a381f
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-16 14:08:37 +0000

    new changes

--- Programs/functions.c
+++ Programs/functions.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-/*
+
+void sayHi(char name[], int age) // void means the function doe not return any value
+{
+  printf("Hello %s, of age %d \n", name, age);
+}
+
+
+
 double cube(double num) // outputs and accepts a double type number
 {
   double result = num * num * num;
   return result;
-  // or
+  // or use
   // return num * num * num;
 }
-*/
+
 
 int max(int num1, int num2, int num3)
 {
@@ -21,20 +28,14 @@ int max(int num1, int num2, int num3)
   return result;
 }
 
+
 int main()
 {
-  // sayHi("John", 40);
-  // sayHi("Jenny", 39);
+  sayHi("John", 40);
+  sayHi("Jenny", 39);
 
-  // printf("Answer: %.4f \n", cube(5.0));
+  printf("Answer: %.4f \n", cube(5.0));
 
   printf("Max: %d \n", max(10, 19, 12));
-  return 0;
-  
+  return 0; 
 }
-
-// void means the function doe not return any value
-// void sayHi(char name[], int age)
-// {
-//   printf("Hello %s, of age %d \n", name, age);
-// }
