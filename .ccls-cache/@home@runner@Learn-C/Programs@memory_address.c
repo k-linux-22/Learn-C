664076b3d3b695cb56c7192856f775a3facaa07a
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-18 10:29:56 +0000

    new changes

--- .ccls-cache/@home@runner@Learn-C/Programs@memory_address.c
+++ .ccls-cache/@home@runner@Learn-C/Programs@memory_address.c
@@ -1,3 +1,5 @@
+// a Pointer is a type of data that we can use in our programs for storing a Memory Address
+
 #include <stdio.h>
 
 int main()
@@ -9,6 +11,16 @@ int main()
   printf("Age stored at: \t %p \n", &age); // %p gives an address (or pointer)
   printf("Weight stored at: \t %p \n", &weight);
   printf("Gender stored at: \t %p \n", &gender);
+
+  int* ptrAge = &age; // pointer variable to store memory address of age variable
+  double* ptrWeight = &weight;
+  char* ptrGender = &gender;
+
+  printf("Data in pointer variable: %p \n", ptrAge);
+  printf("Dereferenced output: %d \n", *ptrAge); // dereference a pointer by using *; output = value stored at the memory address stored in pointer
+
+  // can also use
+  printf("%d \n", *&age);
   
   return 0;
 }
