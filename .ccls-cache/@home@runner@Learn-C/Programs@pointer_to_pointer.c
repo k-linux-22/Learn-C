3522f248150b463747661b24339816a18a9564a2
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-18 10:00:50 +0000

    changes

--- .ccls-cache/@home@runner@Learn-C/Programs@pointer_to_pointer.c
+++ .ccls-cache/@home@runner@Learn-C/Programs@pointer_to_pointer.c
@@ -38,29 +38,29 @@ int main() {
 
 Output - 
 
-&x = Address of x: 	 -375827084 
+&x = Address of x: 	 0x7ffdc424bbe4 
 x = Data in x: 	 10 
 
-&a = Address of a: 	 -375827080 
-a = Address &x -> Data in a: 	 -375827084 
+&a = Address of a: 	 0x7ffdc424bbe8 
+a = Address &x -> Data in a: 	 0x7ffdc424bbe4 
 *a = Data in x -> Address &x -> Data in a: 	 10 
 
-&b = Address of b: 	 -375827072 
-b = Address &a -> Data in b: 	 -375827080 
-*b = Address &x -> Data in a -> Address &a -> Data in b: 	 -375827084 
+&b = Address of b: 	 0x7ffdc424bbf0 
+b = Address &a -> Data in b: 	 0x7ffdc424bbe8 
+*b = Address &x -> Data in a -> Address &a -> Data in b: 	 0x7ffdc424bbe4 
 **b = Data in x -> Address &x -> Data in a -> Address &a -> Data in b: 	 10 
 
-&c = Address of c: 	 -375827064 
-c = Address &b -> Data in c: 	 -375827072 
-*c = Address &a -> Data in b -> Address &b -> Data in c: 	 -375827080 
-**c = Address &x -> Data in a -> Address &a -> Data in b -> Address &b -> Data in c: 	 -375827084 
+&c = Address of c: 	 0x7ffdc424bbf8 
+c = Address &b -> Data in c: 	 0x7ffdc424bbf0 
+*c = Address &a -> Data in b -> Address &b -> Data in c: 	 0x7ffdc424bbe8 
+**c = Address &x -> Data in a -> Address &a -> Data in b -> Address &b -> Data in c: 	 0x7ffdc424bbe4 
 ***c = Data in x -> Address &x -> Data in a -> Address &a -> Data in b -> Address &b -> Data in c: 	 10 
 
-&d = Address of d:	  -375827056 
-d = Address &c -> Data in d: 	 -375827064 
-*d = Address &b -> Data in c -> Address &c -> Data in d: 	 -375827072 
-**d = Address &a -> Data in b -> Address &b -> Data in c -> Address &c -> Data in d: 	 -375827080 
-***d = Address &x -> Data in a -> Address &a -> Data in b -> Address &b -> Data in c -> Address &c -> Data in d: 	 -375827084 
+&d = Address of d:	  0x7ffdc424bc00 
+d = Address &c -> Data in d: 	 0x7ffdc424bbf8 
+*d = Address &b -> Data in c -> Address &c -> Data in d: 	 0x7ffdc424bbf0 
+**d = Address &a -> Data in b -> Address &b -> Data in c -> Address &c -> Data in d: 	 0x7ffdc424bbe8 
+***d = Address &x -> Data in a -> Address &a -> Data in b -> Address &b -> Data in c -> Address &c -> Data in d: 	 0x7ffdc424bbe4 
 ****d = Data in x -> Address &x -> Data in a -> Address &a -> Data in b -> Address &b -> Data in c -> Address &c -> Data in d: 	 10 
 
 
