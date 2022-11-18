989ebc28ca4e5fd9a1f4b32acccec487834ddae7
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-18 08:19:02 +0000

    changes

--- Programs/guess_game.c
+++ Programs/guess_game.c
@@ -2,5 +2,28 @@
 
 int main()
 {
-  int secretNum = 0;  
+  int secretNum = 5;
+  int guess;
+  int guessCount = 0;
+  int guessLimit = 3;
+  int outOfGuesses = 0;
+  
+  while(guess != secretNum && outOfGuesses == 0)
+  {
+    if(guessCount < guessLimit)
+    {
+      printf("Enter a number: ");
+      scanf("%d", &guess);
+      guessCount++;
+    }
+    else
+    {
+      outOfGuesses = 1;
+    }
+  }
+
+  if (outOfGuesses == 1) printf("Out of Guesses \n");
+  else printf("You Win! \n");
+  
+  return 0;
 }
