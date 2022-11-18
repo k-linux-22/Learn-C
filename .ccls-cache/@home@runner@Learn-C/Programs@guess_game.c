c15dcdabdba2966f50e5c723091d33be6fc6ad83
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-18 08:05:47 +0000

    changes

---
+++ .ccls-cache/@home@runner@Learn-C/Programs@guess_game.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+  int secretNum = 0;  
+}
--- .ccls-cache/@home@runner@Learn-C/Programs@while_loop.c
+++ .ccls-cache/@home@runner@Learn-C/Programs@while_loop.c
@@ -3,8 +3,19 @@
 int main()
 {
   int index = 1;
-  while(index <= 5){
+  
+  while(index <= 5)
+  {
     printf("%d \n", index);
     index++;
   }
+
+  index = 1;
+  do
+  {
+    printf("%d \n", index);
+    index++;
+  } while(index <= 5);
+
+  return 0;
 }
