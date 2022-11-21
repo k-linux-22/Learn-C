b655ae334023e0dfd78da9b539c71382b52731b0
K Linux <108090286+k-linux-22@users.noreply.github.com>
2022-11-21 07:10:35 +0000

    changes

--- Programs/writing_files.c
+++ Programs/writing_files.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-  FILE * fpointer = 
+  FILE * fpointer = fopen("~/Learn-C/people.txt", "w"); // w = write a file
 
+  fprintf(fpointer, "Jim, Salesman\nPam, Receptionist\nOscar, Accounting");
+
+  // fprintf(fpointer, "overridden"); // overrides the above data in the file
+  
+  
+  fclose(fpointer); // always remember to close a file
   return 0;
 }
